6a39d2f08f4a4b61dc4289898a4625a90795abd4
kinyth <111610334+kinyth@users.noreply.github.com>
2022-12-05 22:39:37 -0800

    add comments

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,59 @@
 import java.util.Random;
 import java.util.Scanner;
- 
+
 public class Main {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    Random random = new Random();
- 
-    while (true) {
-      System.out.print("Do you want to play? (y/n) ");
-      String answer = scanner.nextLine();
-      if (answer.equalsIgnoreCase("n")) {
-        break;
-      }
- 
-      int pileSize = random.nextInt(41) + 10;
-      System.out.println("Starting game with " + pileSize + " pieces in the pile");
- 
-      while (pileSize > 0) {
-        System.out.println("There are " + pileSize + " pieces in the pile");
-        System.out.println("Choose how many pieces to take (1-" + (pileSize / 2) + "): ");
-        int piecesToTake = Integer.parseInt(scanner.nextLine());
- 
-        while (piecesToTake < 1 || piecesToTake > pileSize / 2) {
-          System.out.println("Invalid input. Please choose a number between 1 and " + (pileSize / 2));
-          piecesToTake = Integer.parseInt(scanner.nextLine());
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        // Get player names
+        System.out.print("Enter player 1's name: ");
+        String player1 = scanner.nextLine();
+        System.out.print("Enter player 2's name: ");
+        String player2 = scanner.nextLine();
+
+        // Initialize game variables
+        int pileSize = random.nextInt(41) + 10; // Random number between 10 and 50
+        int currentPlayer = random.nextInt(2) + 1; // Random starting player
+        boolean gameOver = false;
+
+        // Play game
+        while (!gameOver) {
+            // Print current game state
+            System.out.println("Pile size: " + pileSize);
+            if (currentPlayer == 1) {
+                System.out.println("Current player: " + player1);
+            } else {
+                System.out.println("Current player: " + player2);
+            }
+
+            // Get player's move
+            System.out.print("Enter number of pieces to take (1-" + (pileSize / 2) + "): ");
+            int move = scanner.nextInt();
+
+            // Validate player's move
+            if (move < 1 || move > pileSize / 2) {
+                System.out.println("Invalid move. Try again.");
+                continue;
+            }
+
+            // Update game state
+            pileSize -= move;
+            if (currentPlayer == 1) {
+                currentPlayer = 2;
+            } else {
+                currentPlayer = 1;
+            }
+            gameOver = (pileSize == 1);
         }
- 
-        pileSize -= piecesToTake;
-      }
- 
-      System.out.println("You lose!");
-    }
-  }
+
+        // Announce winner
+        if (currentPlayer == 1) {
+            System.out.println("Game over! " + player1 + " wins!");
+        } else {
+            System.out.println("Game over! " + player2 + " wins!");
+        }
+
+        // Offer to play again
+}
 }
